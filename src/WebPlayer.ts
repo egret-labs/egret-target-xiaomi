b248a8268f78f7ecb3945e5a22cf6520e460e8ed
fengtao <fengtao@egret.com>
2019-09-12 13:53:13 +0800

    WebFps support egret.log egret.warn egret.error

--- src/WebPlayer.ts
+++ src/WebPlayer.ts
@@ -116,7 +116,7 @@ namespace egret.qgame {
             }
             option.fpsStyles = styles;
 
-            option.showLog = false;
+            option.showLog = options.showLog;
             option.logFilter = "";
 
             return option;
